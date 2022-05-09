a5b926b3938b94ee97f55eaac934630ed93d7e76
DakuNaku <Zac_Reichelt@hotmail.com>
2022-05-09 14:19:21 +1000

    minor updatas
    testing stuff out

---
+++ Assets/Scripts/parentSystem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parentSystem : MonoBehaviour
+{
+    public GameObject thruster;
+    public GameObject conneter;
+    public GameObject fuel;
+    public GameObject tip;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
