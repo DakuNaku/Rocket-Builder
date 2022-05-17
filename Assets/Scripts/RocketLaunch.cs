e2ae1eb5b3d2994770badd7b0086389bf0bdb4d4
Zac <Zac_Reichelt@hotmail.com>
2022-05-18 09:02:11 +1000

    mouse click working on 3d objects

---
+++ Assets/Scripts/RocketLaunch.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLaunch : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //A physics hit object to store info im going to get about where the ray hit
+            RaycastHit hitinfo;
+
+            //the distance of the ray the i'm using
+            float distanceofRay = 100;
+
+            //Makes the ray from ray cast the mouse loctaion (hopefully)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Raycast to where the mouse is
+            Physics.Raycast(ray, out hitinfo, distanceofRay);
+
+            if (hitinfo.transform.tag == "tip")
+            {
+                Debug.Log("raycast HIT");
+            }
+            else if (hitinfo.transform.tag == "conneter")
+            {
+                Debug.Log("conneter online");
+            }
+            else if (hitinfo.transform.tag == "rocket control")
+            {
+                Debug.Log("Rocket control online");
+            }
+            else if (hitinfo.transform.tag == "fuel")
+            {
+                Debug.Log("fuel online");
+            }
+            else if (hitinfo.transform.tag == "thruster")
+            {
+                Debug.Log("thruster's are a go for launch");
+            }
+        }
+
+    }
+}
